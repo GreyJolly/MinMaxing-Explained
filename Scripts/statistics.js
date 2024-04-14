0a87417461684f71bc2a7964b89718e1a7d4d6e4
GreyJolly <vortex.minecraft@libero.it>
2024-04-14 14:52:45 +0200

    Added Statistics page

--- Scripts/statistics.js
+++ Scripts/statistics.js
@@ -4,26 +4,98 @@ const API_get_URL = 'http://localhost:3000/api/data/get_value';
 
 // GLOBAL VARIABLES
 
-var winrates;
+var api_data;
 
 //==================================
 // MAIN FUNCTIONS
 //==================================
 
-function initialize() {
+var xChart,
+	playerChart,
+	randomChart,
+	minmaxerChart;
 
+function initialize() {
+	updateDataFromAPI();
+
+	const x_chart_context = document.getElementById('x_chart').getContext('2d');
+	const player_chart_context = document.getElementById('player_chart').getContext('2d');
+	const random_chart_context = document.getElementById('random_chart').getContext('2d');
+	const minmaxer_chart_context = document.getElementById('minmaxer_chart').getContext('2d');
+
+	const xData = {
+		labels: ['Wins', 'Ties', 'Losses'],
+		datasets: [{
+			data: [0, 0, 0],
+			backgroundColor: ['#ff6384', '#36a2eb', '#ffce56'],
+		}],
+	};
+	const playerData = {
+		labels: ['Wins', 'Ties', 'Losses'],
+		datasets: [{
+			data: [0, 0, 0],
+			backgroundColor: ['#ff6384', '#36a2eb', '#ffce56'],
+		}],
+	};
+	const randomData = {
+		labels: ['Wins', 'Ties', 'Losses'],
+		datasets: [{
+			data: [0, 0, 0],
+			backgroundColor: ['#ff6384', '#36a2eb', '#ffce56'],
+		}],
+	};
+	const minmaxerData = {
+		labels: ['Wins', 'Ties', 'Losses'],
+		datasets: [{
+			data: [0, 0, 0],
+			backgroundColor: ['#ff6384', '#36a2eb', '#ffce56'],
+		}],
+	};
+
+	xChart = new Chart(x_chart_context, {
+		type: 'pie',
+		data: xData,
+	});
+	playerChart = new Chart(player_chart_context, {
+		type: 'pie',
+		data: playerData,
+	});
+	randomChart = new Chart(random_chart_context, {
+		type: 'pie',
+		data: randomData,
+	});
+	minmaxerChart = new Chart(minmaxer_chart_context, {
+		type: 'pie',
+		data: minmaxerData,
+	});
+	
+ 	setTimeout(refresh, 500); // If you don't wait for the page to fully load it doesn't work
 }
 
 function refresh() {
-	console.log(updateWinratesFromAPI());
-	document.getElementById("x_winrate").innerHTML = winrates.x_winrate;
+	api_data = updateDataFromAPI();
+	
+	// Update the chart data
+	xChart.data.datasets[0].data = [api_data.x_winrate, api_data.x_tierate, 100 - (api_data.x_winrate + api_data.x_tierate)];
+	playerChart.data.datasets[0].data = [api_data.player_winrate, api_data.player_tierate, (100 - (api_data.player_winrate + api_data.player_tierate))];
+	randomChart.data.datasets[0].data = [api_data.random_winrate, api_data.random_tierate, (100 - (api_data.random_winrate + api_data.random_tierate))];
+	minmaxerChart.data.datasets[0].data = [api_data.minmaxer_winrate, api_data.minmaxer_tierate, 100 - (api_data.minmaxer_winrate + api_data.minmaxer_tierate)];
+
+	// Refresh the charts
+	xChart.update();
+	playerChart.update();
+	randomChart.update();
+	minmaxerChart.update();
 }
+
+
+
 
 //==================================
 // API HANDLING
 //==================================
 
-function updateWinratesFromAPI() {
+function updateDataFromAPI() {
 
 	fetch(API_get_URL, {
 		method: 'GET',
@@ -38,9 +110,9 @@ function updateWinratesFromAPI() {
 			return response.json();
 		})
 		.then(data => {
-			winrates = data;
-			return winrates;
+			api_data = data;
+			return api_data;
 		})
 
-	return winrates;
+	return api_data;
 }
